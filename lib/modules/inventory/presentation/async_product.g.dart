60306ebe66006f321e4fd87b0b50c49e29430073
armandosudi <armando.sudi@gmail.com>
2024-02-06 18:06:50 +0100

    Implement the product with different fields for the inventory

---
+++ lib/modules/inventory/presentation/async_product.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'async_product.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$asyncProductHash() => r'e2bcb77d7e3d147b9af7641977f003efe7681f0d';
+
+/// See also [AsyncProduct].
+@ProviderFor(AsyncProduct)
+final asyncProductProvider =
+    AutoDisposeAsyncNotifierProvider<AsyncProduct, List<Product>>.internal(
+  AsyncProduct.new,
+  name: r'asyncProductProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$asyncProductHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$AsyncProduct = AutoDisposeAsyncNotifier<List<Product>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
